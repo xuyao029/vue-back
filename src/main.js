ccc4df2113253cf1cca6555339c9b1c3177462ef
xuyao029 <632434521@qq.com>
2020-08-19 14:46:11 +0800

    登录功能

--- src/main.js
+++ src/main.js
@@ -3,6 +3,13 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './plugins/element.js'
+//导入全局样式表
+import './assets/css/global.css'
+import './assets/fonts/iconfont.css'
+
+import axios from 'axios'
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
 
